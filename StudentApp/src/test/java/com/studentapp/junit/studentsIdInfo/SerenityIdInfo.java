65c8ac1c8b563bd34524b77f71ff93748615774c
Tiesha Leacock <tiesha.leacock@hyuna.bb>
2018-06-20 13:07:36 -0400

    Commiting the StudentApp project

---
+++ StudentApp/src/test/java/com/studentapp/junit/studentsIdInfo/SerenityIdInfo.java
@@ -0,0 +1,89 @@
+package com.studentapp.junit.studentsIdInfo;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import io.restassured.RestAssured;
+import net.serenitybdd.junit.runners.SerenityRunner;
+import net.serenitybdd.rest.SerenityRest;
+import net.thucydides.core.annotations.Manual;
+import net.thucydides.core.annotations.Pending;
+
+@RunWith(SerenityRunner.class)
+public class SerenityIdInfo {
+	
+	@BeforeClass
+	public static void init() {
+		RestAssured.baseURI="http://localhost:8080/student";
+	}
+	
+	@Test
+	public void getAllStudents2() {
+		SerenityRest.given()
+		.when()
+		.get("/list")
+		.then()
+		.log()
+		.all()
+		.statusCode(200);
+	}
+	
+	@Test
+	public void thisIsAfailing() {
+		SerenityRest.given()
+		.when()
+		.get("/list")
+		.then()
+		.log()
+		.all()
+		.statusCode(500);
+	}
+	
+	@Pending
+	@Test
+	public void thisIsAPending() {
+		SerenityRest.given()
+		.when()
+		.get("/list")
+		.then()
+		.log()
+		.all()
+		.statusCode(200);
+	}
+
+	@Ignore
+	@Test
+	public void thisIsAskippedTest() {
+		SerenityRest.given()
+		.when()
+		.get("/list")
+		.then()
+		.log()
+		.all()
+		.statusCode(200);
+	}
+
+	@Test
+	public void thisIsATestWithError() {
+		System.out.println("This is an error" + (5/0));
+	}
+	
+	@Test
+	public void fileDoesNotExist() throws FileNotFoundException{
+		File file = new File("E://file.txt");
+		FileReader fr = new FileReader(file);
+	}
+	
+	@Manual
+	@Test
+	public void thisIsAManualTest() {
+		
+	}
+}
+
